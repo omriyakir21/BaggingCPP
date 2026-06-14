a0388ab672dd97bc8ac73aff4ff49667355d9def
omriyakir21 <omriyakir21@gmail.com>
2026-06-14 09:43:54 +0300

    ready for test run

--- data_preparation/cross_validation.py
+++ data_preparation/cross_validation.py
@@ -1,4 +1,6 @@
 
+import os
+
 import data_preparation.cross_validation_utils as cv_utils
 import argparse
 
@@ -12,4 +14,5 @@ if __name__ == '__main__':
     data_for_training_dir = args.data_for_training_dir
     full_dataset_path = args.full_dataset_path
     num_folds = args.num_folds
+    os.makedirs(data_for_training_dir, exist_ok=True)
     cv_utils.partition_to_folds_and_save(data_for_training_dir=data_for_training_dir, dataset_path = full_dataset_path, num_folds=num_folds) 
